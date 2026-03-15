43bc51a10531521576f16c8ee86b4dbc7746c9f3
Elryan Gomes <elryangomes@gmail.com>
2026-03-14 21:54:33 -0300

    Inserindo a pagina de editar

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Renderiza o Dashboard
     return render_template('index.html')
 
 @app.route('/editar')
 def editar():
+    # Renderiza a página de edição/cadastro
     return render_template('editar.html')
 
-
-
-
-
 if __name__ == "__main__":
+    # debug=True reinicia o servidor automaticamente ao salvar o arquivo
     app.run(port=5000, debug=True)
